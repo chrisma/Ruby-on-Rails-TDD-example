a48e00581f15cd3a97e3169e38009a88a2f469e1
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2015-04-24 16:07:20 +0200

    Generate ContactController

--- Contacts/spec/controllers/contact_controller_spec.rb
+++ Contacts/spec/controllers/contact_controller_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ContactController, type: :controller do
-  describe "GET index" do
-    it "assigns @contacts in the context" do
-      contact = Contact.create
+  describe "GET #index" do
+    it "returns HTTP success" do
       get :index
-      expect(assigns(:contact_list)).to eq([contact])
+      expect(response).to have_http_status(:success)
     end
 
     it "renders the index template" do
       get :index
-      expect(response).to render_template("index")
+      expect(response).to render_template("contact/index")
+    end
+    
+    it "assigns @contact_list in the controller context" do
+      contact = Contact.create
+      get :index
+      expect(assigns(:contact_list)).to eq([contact])
     end
   end
 end
