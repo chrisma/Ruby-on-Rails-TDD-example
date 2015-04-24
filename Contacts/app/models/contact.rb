d9f3ebe440ccf832d68f895f0af2e34b107ad535
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2015-04-24 14:48:02 +0200

    Naive implementation for model test.

--- Contacts/app/models/contact.rb
+++ Contacts/app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ActiveRecord::Base
+
+	def age
+		age = Date.today.year - date_of_birth.year
+		age -= 1 if Date.today < date_of_birth + age.years #for days before birthday
+		return age
+	end
+
 end
---
+++ Contacts/spec/models/contact_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Contact, type: :model do
+  before(:each) do
+    @contact = Contact.new(first_name:'Adam', date_of_birth:Date.today)
+  end
+
+  context "age calculation" do
+    it "calculates the correct age for newborns" do
+      expect(@contact.age).to eq(0)
+    end
+
+    it "updates correctly on date_of_birth changes" do
+      expect { 
+        @contact.date_of_birth = Date.today - 5.years 
+      }.to change{@contact.age}.from(0).to(5)
+    end
+
+    it "calculates correctly in full years" do
+      @contact.date_of_birth = Date.today - 1.year
+      expect(@contact.age).to eq(1)
+    end
+
+    it "calculates correctly in between years" do
+      @contact.date_of_birth = Date.today - 400.days
+      expect(@contact.age).to eq(1)
+    end
+
+    it "handles dates of birth in the future" do
+      @contact.date_of_birth = Date.today + 1.year
+      expect(unlikely_contact.age).to eq(0)
+    end
+
+  end
+end
