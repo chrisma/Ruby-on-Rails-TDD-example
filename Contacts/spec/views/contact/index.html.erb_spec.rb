004098debe69111bc85eb34297a42ee986b4f5cb
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2015-04-24 16:45:14 +0200

    Made view test green

--- Contacts/spec/views/contact/index.html.erb_spec.rb
+++ Contacts/spec/views/contact/index.html.erb_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "contact/index.html.erb", type: :view do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  context "with 2 contacts" do
+    before(:each) do
+      @adam = Contact.create!(first_name: "Adam")
+      @eve = Contact.create!(first_name: "Eve")
+      assign(:contact_list, [@adam, @eve])
+      render # populates rendered
+    end
+
+    it "displays all first names of contacts" do
+      expect(rendered).to match @adam.first_name
+      expect(rendered).to match @eve.first_name
+    end
+
+	it "displays a heading" do
+      expect(rendered).to match "<h1>Contact list</h1>"
+    end
+  end
 end
