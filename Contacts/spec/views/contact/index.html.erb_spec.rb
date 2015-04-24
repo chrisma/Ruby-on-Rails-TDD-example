7afb69a538e673e00547d002b51d699e5ebcf242
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2015-04-24 17:12:46 +0200

    include computed values in view test.

--- Contacts/spec/views/contact/index.html.erb_spec.rb
+++ Contacts/spec/views/contact/index.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "contact/index.html.erb", type: :view do
   context "with 2 contacts" do
     before(:each) do
-      @adam = Contact.create!(first_name: "Adam")
-      @eve = Contact.create!(first_name: "Eve")
+      @adam = Contact.create!(first_name: "Adam", date_of_birth:Date.today - 25.years)
+      @eve = Contact.create!(first_name: "Eve", date_of_birth:Date.today - 19.years)
       assign(:contact_list, [@adam, @eve])
       render # populates rendered
     end
@@ -12,6 +12,11 @@ RSpec.describe "contact/index.html.erb", type: :view do
     it "displays all first names of contacts" do
       expect(rendered).to match @adam.first_name
       expect(rendered).to match @eve.first_name
+    end
+
+    it "displays all first names of contacts" do
+      expect(rendered).to match @adam.age.to_s
+      expect(rendered).to match @eve.age.to_s
     end
 
 	it "displays a heading" do
