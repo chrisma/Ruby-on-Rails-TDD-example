7a31b4e569a819b5bebea9de628760199a3e9566
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2015-04-24 15:01:16 +0200

    Make model test green

--- Contacts/app/models/contact.rb
+++ Contacts/app/models/contact.rb
@@ -3,6 +3,7 @@ class Contact < ActiveRecord::Base
 	def age
 		age = Date.today.year - date_of_birth.year
 		age -= 1 if Date.today < date_of_birth + age.years #for days before birthday
+		age = 0 if age < 0
 		return age
 	end
 
