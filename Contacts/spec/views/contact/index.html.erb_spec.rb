962d41a6af01629674c90884141354f3e98add0a
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2015-05-12 23:05:02 +0200

    Update view test.

--- Contacts/spec/views/contact/index.html.erb_spec.rb
+++ Contacts/spec/views/contact/index.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "contact/index.html.erb", type: :view do
   context "with 2 contacts" do
     before(:each) do
-      @adam = Contact.create!(first_name: "Adam", date_of_birth:Date.today - 25.years)
-      @eve = Contact.create!(first_name: "Eve", date_of_birth:Date.today - 19.years)
+      @adam = Contact.create!(first_name: "Adam")
+      @eve = Contact.create!(first_name: "Eve")
       assign(:contact_list, [@adam, @eve])
       render # populates rendered
     end
@@ -14,12 +14,7 @@ RSpec.describe "contact/index.html.erb", type: :view do
       expect(rendered).to match @eve.first_name
     end
 
-    it "displays all first names of contacts" do
-      expect(rendered).to match @adam.age.to_s
-      expect(rendered).to match @eve.age.to_s
-    end
-
-	it "displays a heading" do
+    it "displays a heading" do
       expect(rendered).to match "<h1>Contact list</h1>"
     end
   end
