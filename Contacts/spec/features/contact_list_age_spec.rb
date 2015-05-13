b2ec3e153e61b6e6a36cdcb9e59ba7829dd74f23
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2015-05-13 11:18:58 +0200

    More test cases for feature spec

--- Contacts/spec/features/contact_list_age_spec.rb
+++ Contacts/spec/features/contact_list_age_spec.rb
@@ -16,15 +16,27 @@ describe "the contact list", :type => :feature do
   it "shows the names of contacts and their age" do
     @contacts.each do |c|
       line = c.first_name + ' ' + c.last_name + ' | ' + c.age.to_s
-      expect(page).to have_text line
+      expect(page).to have_content line
     end
   end
 
   it "shows the correct amount of list items" do
-    expect(page.all("ul li").size).to eq @contacts.size
+    expect(page).to have_css("li", :count => @contacts.size)
+  end
+
+  it "Does not render the 'empty' message" do
+    expect(page).to have_no_content 'No contacts available'
   end
 
   it "has a correct title" do
     expect(page).to have_title 'Contacts'
+  end
+
+  it "has a single top-level heading" do
+    expect(page).to have_xpath("//h1", :count => 1)
+  end
+
+  it "saves the page HTML for debugging" do
+    save_and_open_page
   end
 end
