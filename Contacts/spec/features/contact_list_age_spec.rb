fdf05fbddd191a4a4c009c9c145a0f0b7bcd6083
Christoph Matthies <christoph.matthies@student.hpi.uni-potsdam.de>
2015-05-18 14:44:16 +0200

    Comments for feature spec.

--- Contacts/spec/features/contact_list_age_spec.rb
+++ Contacts/spec/features/contact_list_age_spec.rb
@@ -14,6 +14,8 @@ describe "the contact list", :type => :feature do
   end
 
   it "shows the names of contacts and their age" do
+    # Only check that the text is present, the correctness of
+    # age calculation is explicitly tested in the model test
     @contacts.each do |c|
       line = c.first_name + ' ' + c.last_name + ' | ' + c.age.to_s
       expect(page).to have_content line
@@ -25,6 +27,10 @@ describe "the contact list", :type => :feature do
   end
 
   it "saves the page HTML for debugging" do
+    # This isn't really a test, it will save the HTML just as
+    # the test sees it to Contacts/tmp/capybara/*.html,
+    # helpful for debugging
+    skip "Save HTML, should be skipped" # skip the test
     save_and_open_page
   end
 end
